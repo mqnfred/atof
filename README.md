8639b2bb5e6f87cc51dd411e1a5d4bdbf58b89af
Louis Feuvrier <mqnfred@gmail.com>
2020-07-29 10:45:10 -0700

    Fixup setup.sh to use installed rustup

--- README.md
+++ README.md
@@ -11,10 +11,10 @@ your system at those tools (after install and before any development session),
 you need to `source activate.sh`.
 
 Once that is done, you will be able to proceed with building/testing the whole
-stack without any need for an internet connexion.
+stack without any further need for an internet connexion.
 
 ### Parameters
 
 `./setup.sh` defines defaults for where to install tools, which version to
-install etc. You can override this using environment variable. To see the list
+install etc. You can override this using environment variables. To see the list
 of knobs and their defaults, `egrep '^test -z' ./setup.sh`.
--- setup.sh
+++ setup.sh
@@ -12,9 +12,10 @@ test -z "$ACTIVATE"		&& ACTIVATE=activate.sh
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o "$RUSTUP_INSTALLER"
 chmod +x "$RUSTUP_INSTALLER"
 
-# install rustup
+# install basic rust suite (rustup, rustc, cargo...) and set path accordingly
 export RUSTUP_INIT_SKIP_PATH_CHECK=yes
 "$RUSTUP_INSTALLER" -y --no-modify-path --default-toolchain none --profile minimal
+export PATH="$CARGO_HOME/bin:$PATH"
 
 # install rustc with explicit targets
 rustup toolchain install "$RUST_VERSION"
@@ -22,10 +23,11 @@ for TARGET in $RUST_TARGETS; do
 	rustup target install "$TARGET"
 done
 
+# generate activation script
 cat <<EOF > "$ACTIVATE"
 #!/bin/bash
 
 export RUSTUP_HOME="$RUSTUP_HOME"
 export CARGO_HOME="$CARGO_HOME"
-export PATH="$CARGO_HOME/bin:$PATH"
+export PATH="$CARGO_HOME/bin:\$PATH"
 EOF
