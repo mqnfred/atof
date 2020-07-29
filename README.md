98fa2a2140204e751fe97690fbdd4c1b1176266a
Louis Feuvrier <mqnfred@gmail.com>
2020-07-29 09:19:02 -0700

    Initial commit with README.md and setup.sh

---
+++ README.md
@@ -0,0 +1,20 @@
+# ATOF("4") = 4.0
+
+Welcome to the jungle
+
+## Setup tools
+
+### Basics
+
+After cloning the repository, go ahead and `./setup.sh` all the tools. To point
+your system at those tools (after install and before any development session),
+you need to `source activate.sh`.
+
+Once that is done, you will be able to proceed with building/testing the whole
+stack without any need for an internet connexion.
+
+### Parameters
+
+`./setup.sh` defines defaults for where to install tools, which version to
+install etc. You can override this using environment variable. To see the list
+of knobs and their defaults, `egrep '^test -z' ./setup.sh`.
---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+test -z "$TOOLS_DIR"		&& TOOLS_DIR="tools"; mkdir -p "$TOOLS_DIR"
+test -z "$RUSTUP_INSTALLER"	&& RUSTUP_INSTALLER="$TOOLS_DIR/rustup-init.sh"
+test -z "$RUSTUP_HOME"		&& RUSTUP_HOME="$TOOLS_DIR/rustup"
+test -z "$CARGO_HOME"		&& CARGO_HOME="$TOOLS_DIR/cargo"
+test -z "$RUST_TARGETS"		&& RUST_TARGETS="x86_64-unknown-linux-gnu x86_64-apple-darwin"
+test -z "$RUST_VERSION"		&& RUST_VERSION="1.45.0"
+test -z "$ACTIVATE"		&& ACTIVATE=activate.sh
+
+# pull rustup installer, for more info visit https://rustup.rs
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o "$RUSTUP_INSTALLER"
+chmod +x "$RUSTUP_INSTALLER"
+
+# install rustup
+export RUSTUP_INIT_SKIP_PATH_CHECK=yes
+"$RUSTUP_INSTALLER" -y --no-modify-path --default-toolchain none --profile minimal
+
+# install rustc with explicit targets
+rustup toolchain install "$RUST_VERSION"
+for TARGET in $RUST_TARGETS; do
+	rustup target install "$TARGET"
+done
+
+cat <<EOF > "$ACTIVATE"
+#!/bin/bash
+
+export RUSTUP_HOME="$RUSTUP_HOME"
+export CARGO_HOME="$CARGO_HOME"
+export PATH="$CARGO_HOME/bin:$PATH"
+EOF
